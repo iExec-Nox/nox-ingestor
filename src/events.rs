fbe4b6ab554c4231bee1bf151f89c0ea56ce2222
Natchica <nathan.gaud@gmail.com>
2026-02-12 15:28:19 +0100

    fix: update serialization to camelCase for event `Operator` enum

--- src/events.rs
+++ src/events.rs
@@ -58,7 +58,7 @@ pub struct EncryptionOperation {
 
 /// Event payload with typed variants
 #[derive(Debug, Clone, Serialize, Deserialize)]
-#[serde(tag = "type", rename_all = "snake_case")]
+#[serde(rename_all = "camelCase", tag = "type")]
 pub enum Operator {
     PlaintextToEncrypted(EncryptionOperation),
     Add(ArithmeticOperation),
